ae7133aabd18e39afae83d35f643b5874018a3c8
Gianluca Jahn <gianluca.jahn98@gmail.com>
2023-11-05 14:51:01 +0100

    Add spring animation to settings button

--- src/app/components/Settings/Settings.tsx
+++ src/app/components/Settings/Settings.tsx
@@ -4,9 +4,7 @@ import { motion } from "framer-motion";
 import classNames from "classnames";
 import Image from "next/image";
 
-interface SettingsProps {
-
-}
+interface SettingsProps {}
 
 export default function Settings(props: SettingsProps) {
   const [isExpanded, setIsExpanded] = useState(false);
@@ -14,21 +12,30 @@ export default function Settings(props: SettingsProps) {
   useEffect(() => {
     console.log(document.body.scrollTop);
     if (document.body.scrollTop >= 1000) {
-        document.getElementById('settings')!.style.backgroundColor = 'white';
+      document.getElementById("settings")!.style.backgroundColor = "white";
     } else {
-
     }
   });
 
   return (
-    <motion.div id="settings" className={classNames(css.settings, {
-        [css.isExpanded]: isExpanded
-    })}>
-        {!isExpanded ? <Image className={css.settingsIcon} alt="Settings" src={require("../../../resources/images/settings.png")} height="36" width="36" /> : (
-            <div className={css.expandedContent}>
-
-            </div>
-        )}
+    <motion.div
+      id="settings"
+      className={classNames(css.settings, {
+        [css.isExpanded]: isExpanded,
+      })}
+      whileHover={{ scale: 1.125, transition: { type: "spring", duration: 0.6, bounce: 0.6 } }}
+    >
+      {!isExpanded ? (
+        <Image
+          className={css.settingsIcon}
+          alt="Settings"
+          src={require("../../../resources/images/settings.png")}
+          height="36"
+          width="36"
+        />
+      ) : (
+        <div className={css.expandedContent}></div>
+      )}
     </motion.div>
   );
 }
