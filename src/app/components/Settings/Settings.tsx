0eb5d066c9c90ef467da720451b7e79b48721b06
Gianluca Jahn <gianluca.jahn98@gmail.com>
2023-12-22 21:24:24 +0100

    Setup settings button functionality

--- src/app/components/Settings/Settings.tsx
+++ src/app/components/Settings/Settings.tsx
@@ -9,30 +9,86 @@ interface SettingsProps {}
 export default function Settings(props: SettingsProps) {
   const [isExpanded, setIsExpanded] = useState(false);
 
+  const toggleSettings = () => {
+    if (isExpanded) {
+      return;
+    }
+    setIsExpanded(true);
+  };
+
   return (
     <motion.div
       id="settings"
+      onClick={toggleSettings}
       className={classNames(css.settings, {
         [css.isExpanded]: isExpanded,
       })}
       whileHover={{
-        scale: 1.125,
+        scale: isExpanded ? 1 : 1.125,
         transition: { type: "spring", duration: 0.6, bounce: 0.6 },
       }}
       whileTap={{
-        scale: 1
+        scale: 1,
+        transition: { type: "none" },
       }}
     >
+      {isExpanded ? (
+        <div className={css.settingsContent}>
+          <div className={css.settingsRow}>
+            <h3>Language</h3>
+            <div className={css.settingsOptions}>
+              <button className={css.settingsOption}>English</button>
+              <button className={css.settingsOption}>German</button>
+            </div>
+          </div>
+
+          <div className={css.settingsRow}>
+            <h3>Color Mode</h3>
+            <div className={css.settingsOptions}>
+              <button className={css.settingsOption}>Light Mode</button>
+              <button className={css.settingsOption}>Dark Mode</button>
+            </div>
+          </div>
+
+          <div className={css.settingsRow}>
+            <h3>Currency</h3>
+            <div className={css.settingsOptions}>
+              <button className={css.settingsOption}>Euro</button>
+              <button className={css.settingsOption}>CHF</button>
+              <button className={css.settingsOption}>USD</button>
+            </div>
+          </div>
+
+          <div className={css.settingsRow}>
+            <h3>Privacy</h3>
+            <div className={css.settingsOptions}>
+              <p>Learn more...</p>
+            </div>
+          </div>
+        </div>
+      ) : null}
       {!isExpanded ? (
         <Image
-          className={css.settingsIcon}
+          className={`${css.settingsIcon} ${css.wheelIcon}`}
+          onClick={() => setIsExpanded(!isExpanded)}
           alt="Settings"
-          src={require("../../../resources/images/settings.png")}
+          src={require(`../../../resources/images/${
+            isExpanded ? "check" : "settings"
+          }.png`)}
           height="36"
           width="36"
         />
       ) : (
-        <div className={css.expandedContent}></div>
+        <Image
+          className={`${css.settingsIcon} ${css.checkIcon}`}
+          onClick={() => setIsExpanded(!isExpanded)}
+          alt="Settings"
+          src={require(`../../../resources/images/${
+            isExpanded ? "check" : "settings"
+          }.png`)}
+          height="28"
+          width="36"
+        />
       )}
     </motion.div>
   );
